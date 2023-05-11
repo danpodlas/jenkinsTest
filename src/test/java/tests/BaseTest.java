7dc5936d27c0e2fd412fd450cdee9025eaf88581
podlas69332 <podlas693>
2023-05-11 14:53:44 +0200

    add tests

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,44 @@
+package tests;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import utils.DriverFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+    protected static ExtentReports extentReports;
+    protected static ExtentHtmlReporter htmlReporter;
+
+    @BeforeSuite
+    public void beforeSuite(){
+        htmlReporter = new ExtentHtmlReporter("index.html");
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(htmlReporter);
+    }
+
+    @AfterSuite
+    public void afterSuite(){
+        htmlReporter.flush();
+        extentReports.flush();
+    }
+    @BeforeMethod
+    public void setup() {
+        driver = DriverFactory.getDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://seleniumdemo.com/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/tests/RegisterTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+import com.aventstack.extentreports.ExtentTest;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class RegisterTest extends BaseTest {
+
+    @Test
+    public void registerUserTest() {
+        int random = (int) (Math.random() * 1000);
+        String username = "test" + random + "@test.pl";
+        ExtentTest extentTest = extentReports.createTest("Register user with username " + username);
+        extentTest.pass("Browser opened");
+        WebElement dashboardLink = new HomePage(driver)
+                .openMyAccountPage()
+                .registerUserValidData("test" + random + "@test.pl", "test@test.pl")
+                .getDashboardLink();
+        extentTest.pass("Register steps ");
+        Assert.assertEquals(dashboardLink.getText(), "Dashboard");
+        extentTest.pass("Dashboard link displayed ");
+    }
+
+    @Test
+    public void registerUserWithSameEmailTest() {
+        ExtentTest extentTest = extentReports.createTest("Register user with username test@test.pl");
+        extentTest.pass("Browser opened");
+        WebElement error = new HomePage(driver)
+                .openMyAccountPage()
+                .registerUserInvalidData("test@test.pl", "test@test.pl")
+                .getError();
+        extentTest.pass("Register steps ");
+        System.out.println(error.getText());
+        Assert.assertTrue(error.getText().contains("An account is already registered with your email address"));
+        extentTest.pass("Error message displayed");
+    }
+
+
+}
